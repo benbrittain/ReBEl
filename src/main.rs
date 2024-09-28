126723b6ea1b8bd978d79d3735dfccdcfb0d4980
Benjamin Brittain <ben@brittain.org>
2024-09-28 12:21:14 -0400

    add clap arguments

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,25 @@ mod types;
 mod client;
 
 use anyhow::Result;
+use clap::Parser;
 use client::*;
 use futures::stream::{self, StreamExt};
 use proto::ActionResult;
 use types::*;
 
+/// RBE load generator
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Max number of requests in-flight
+    #[arg(short, long, default_value_t = 1)]
+    max_requests: usize,
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
+    let args = Args::parse();
+
     let fut = move || async {
         let mut client = RebelClient::new("remote-execution").await?;
 
@@ -32,7 +44,7 @@ async fn main() -> Result<()> {
     };
 
     // Create an infinite unordered buffer of action exections
-    let mut actions = stream::repeat_with(|| fut()).buffer_unordered(100);
+    let mut actions = stream::repeat_with(|| fut()).buffer_unordered(args.max_requests);
 
     while let Some(result) = actions.next().await {
         println!("RESPONSE={:#?}", result);
